70f21bd559bdb6af64dd41a821bc7f62b68e8d0a
Mateo Narvaez <mateonarvaez@correo.itm.edu.co>
2023-10-08 04:33:22 -0500

    Create Clas NaturalPeson

--- Person/DAL/Entities/Entity.cs
+++ Person/DAL/Entities/Entity.cs
@@ -1,11 +1,14 @@
-﻿namespace Person.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial2_NaturalPerson.DAL.Entities
 {
     public class Entity
     {
+        #region
         public Guid Id { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? ModifiedDate { get; set; }
 
-
+        #endregion
     }
 }
---
+++ Person/DAL/Entities/NaturalPerson.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial2_NaturalPerson.DAL.Entities
+{
+    public class NaturalPerson : Entity
+    {
+        #region
+        [Required(ErrorMessage = "El campo {0} es Obligatorio")]
+        [Display(Name ="Ingrese Nombre Completo: ")]
+
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es Obligatorio")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es Obligatorio")]
+        [Display(Name = "Ingrese el año de Nacimiento: ")]
+
+        public int BirthYear {  get; set; }
+        public int Age { get; set; }
+        #endregion
+
+    }
+}
